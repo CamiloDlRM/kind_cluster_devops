d27290dbabde47eff3f4a13d2406c89cd1f3b3f6
CamiloDlRM <zinbak78@gmail.com>
2026-05-14 16:53:43 -0500

    add terraform files

---
+++ main.tf
@@ -0,0 +1,31 @@
+locals {
+  worker_count = 3
+}
+
+resource "kind_cluster" "kind-local-cluster" {
+    name           = "test-cluster"
+    wait_for_ready = true
+
+  kind_config {
+      kind        = "Cluster"
+      api_version = "kind.x-k8s.io/v1alpha4"
+
+      node {
+          role = "control-plane"
+
+          kubeadm_config_patches = [
+              "kind: InitConfiguration\nnodeRegistration:\n  kubeletExtraArgs:\n    node-labels: \"ingress-ready=true\"\n"
+          ]
+
+      }
+
+    dynamic "node" {
+      for_each = range(local.worker_count)
+      content {
+        role = "worker"
+        tag  = "worker-${node.key + 1}"  # node.key empieza en 0, lo ajustamos a 1,2,3
+      }
+    }
+
+  }
+}
